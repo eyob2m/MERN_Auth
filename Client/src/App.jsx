7f135baa8f00a5a64bb5b6f0c2f25167cdc81195
eyob2m <eyob2etemariam@gamil.com>
2023-12-10 00:55:39 +0300

    create header component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,12 +5,13 @@ import Profile from "./pages/Profile"
 import Signup from "./pages/Signup"
 import Signin from "./pages/Signin"
 import About from "./pages/About"
+import Header from "./components/Header"
 
 export default function App() {
   return (
 
     <BrowserRouter>
-
+<Header />
 <Routes>
 
 <Route   exact path="/"  element={ <Home />}/>
