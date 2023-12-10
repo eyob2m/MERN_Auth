ae8604f5c05ff644eb754ff2f37822e00a3f129c
eyob2m <eyob2etemariam@gamil.com>
2023-12-11 01:05:14 +0300

    signup api added

--- Api/index.js
+++ Api/index.js
@@ -2,16 +2,19 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import  UserRouter  from './routes/UserRoutes.js'
+import  authRouter  from './routes/auth.js'
 
 dotenv.config()
 
 
 const app = express()
+
+mongoose.connect("mongodb://localhost:27017").then(()=>console.log("Connected to db")).catch(()=>"Error while connecting")
+
+app.use(express.json())
 app.use('/api/user', UserRouter)
+app.use('/api/auth', authRouter)
 app.listen(4000,()=>{
     console.log("Server started"
     )
 })
-mongoose.connect(process.env.MONGO).then(()=>console.log("Connected to db")).catch(()=>"Error while connecting")
-
-
